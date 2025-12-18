ede0e64d9e354ace4d89c0c9645f0fe0168b2151
TimeoMS <timeo.morsilli@etu.univ-amu.fr>
2025-12-18 21:32:42 +0100

    darkmode

--- js/header.js
+++ js/header.js
@@ -1,7 +1,19 @@
 const burger = document.getElementById("burger-menu");
 const burgerOpened = document.getElementById("burger-opened");
+const darkmode = document.getElementById("darkmode");
+const css = document.getElementsByClassName("css");
 
 burger.onclick = () => {
   burger.classList.toggle("activated");
   burgerOpened.classList.toggle("activated");
+};
+
+darkmode.onclick = () => {
+  if (css[0].href.includes("css/dark.css")) {
+    css[0].href = "css/index.css";
+    css[1].href = "css/headerfooter.css";
+  } else {
+    css[1].href = "";
+    css[0].href = "css/dark.css";
+  }
 };
